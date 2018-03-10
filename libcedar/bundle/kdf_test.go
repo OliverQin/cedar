15819c2b8cc68089fafb283f2765f43dd21e64a1
Oliver Qin <qlrpeach@gmail.com>
2018-03-11 02:32:14 +0800

    Simplify test

--- libcedar/bundle/kdf_test.go
+++ libcedar/bundle/kdf_test.go
@@ -12,11 +12,10 @@ func TestCedarKdf(t *testing.T) {
 	k = cedarKdf{}
 	token := k.generate("MyPassword", "Cedar_Session", 256)
 
-	res := make([]byte, 64)
-	hex.Encode(res, token)
+	res := hex.EncodeToString(token)
 
-	log.Println(string(res))
-	if string(res) != "95203e29f9a30f2561c4076b579cc02c3f0bf73788deca6d45c3a939ec2fe6b9" {
+	log.Println(res)
+	if res != "95203e29f9a30f2561c4076b579cc02c3f0bf73788deca6d45c3a939ec2fe6b9" {
 		panic("kdf did not get expected result")
 	}
 }
