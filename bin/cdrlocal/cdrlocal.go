65a2b9e9cc0070847ed552fbbaaf0f0683c62c24
Oliver Qin <qlrpeach@gmail.com>
2018-05-28 00:19:59 +0800

    Support json config file for cmd tool

--- bin/cdrlocal/cdrlocal.go
+++ bin/cdrlocal/cdrlocal.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/OliverQin/cedar/libcedar/proxy"
@@ -17,18 +19,28 @@ func PrintUsage() {
 	flag.PrintDefaults()
 }
 
+type cedarClientConfig struct {
+	Local      string
+	Remote     string
+	Password   string
+	BufferSize int
+	NumOfConns int
+}
+
 func main() {
 	var helpInfo bool
-	var localService string
-	var remoteService string
+	var localAddr string
+	var remoteAddr string
 	var password string
 	var bufferSize int
+	var configFilename string
 	var numOfConns int
 
-	flag.StringVar(&remoteService, "r", "127.0.0.1:41289", "Remote address string.")
+	flag.StringVar(&remoteAddr, "r", "127.0.0.1:41289", "Remote (cdrserver) address and port, like \"127.0.0.1:41289\".")
 	flag.BoolVar(&helpInfo, "h", false, "Display help info.")
-	flag.StringVar(&localService, "s", "127.0.0.1:1080", "Local service string like \"127.0.0.1:1080\". It should not be used with local port (-l).")
+	flag.StringVar(&localAddr, "s", "127.0.0.1:1080", "Local address and port like \"127.0.0.1:1080\".")
 	flag.StringVar(&password, "p", "123456", "Password for encryption")
+	flag.StringVar(&configFilename, "c", "", "Filename of config file. It overwrites command line parameters.")
 	flag.IntVar(&bufferSize, "b", 100, "Max number of buffers.")
 	flag.IntVar(&numOfConns, "n", 10, "Number of TCP connections.")
 
@@ -39,11 +51,38 @@ func main() {
 		os.Exit(0)
 	}
 
-	fmt.Fprintln(os.Stderr, "Remote: ", remoteService)
-	fmt.Fprintln(os.Stderr, "Local:", localService)
+	if configFilename != "" {
+		data, err := ioutil.ReadFile(configFilename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: cannot load config file.\n")
+			os.Exit(0)
+		}
+
+		conf := cedarClientConfig{}
+		json.Unmarshal(data, &conf)
+
+		if conf.Password != "" {
+			password = conf.Password
+		}
+		if conf.Remote != "" {
+			remoteAddr = conf.Remote
+		}
+		if conf.Local != "" {
+			localAddr = conf.Local
+		}
+		if conf.BufferSize != 0 {
+			bufferSize = conf.BufferSize
+		}
+		if conf.NumOfConns != 0 {
+			numOfConns = conf.NumOfConns
+		}
+	}
+
+	fmt.Fprintln(os.Stderr, "Remote: ", remoteAddr)
+	fmt.Fprintln(os.Stderr, "Local:", localAddr)
 	fmt.Fprintln(os.Stderr, "Running...")
 
-	clt := proxy.NewProxyLocal(password, remoteService, localService, bufferSize)
+	clt := proxy.NewProxyLocal(password, remoteAddr, localAddr, bufferSize)
 	clt.Run(numOfConns)
 
 	blocker := make(chan int)
--- bin/cdrserver/cdrserver.go
+++ bin/cdrserver/cdrserver.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/OliverQin/cedar/libcedar/proxy"
@@ -17,15 +19,23 @@ func PrintUsage() {
 	flag.PrintDefaults()
 }
 
+type cedarServerConfig struct {
+	Remote     string
+	Password   string
+	BufferSize int
+}
+
 func main() {
 	var helpInfo bool
-	var serviceString string
+	var remoteAddr string
 	var password string
 	var bufferSize int
+	var configFilename string
 
 	flag.BoolVar(&helpInfo, "h", false, "Display help info.")
-	flag.StringVar(&serviceString, "s", "127.0.0.1:41289", "Service string like \"127.0.0.1:41289\".")
+	flag.StringVar(&remoteAddr, "s", "127.0.0.1:41289", "Remote (cdrserver) address and port, like \"127.0.0.1:41289\".")
 	flag.StringVar(&password, "p", "123456", "Password for encryption.")
+	flag.StringVar(&configFilename, "c", "", "Filename of config file. It overwrites command line parameters.")
 	flag.IntVar(&bufferSize, "b", 100, "Max number of buffers.")
 
 	flag.Parse()
@@ -34,18 +44,40 @@ func main() {
 		PrintUsage()
 		os.Exit(0)
 	}
-	if serviceString == "" {
+
+	if configFilename != "" {
+		data, err := ioutil.ReadFile(configFilename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: cannot load config file.\n")
+			os.Exit(0)
+		}
+
+		conf := cedarServerConfig{}
+		json.Unmarshal(data, &conf)
+
+		if conf.Password != "" {
+			password = conf.Password
+		}
+		if conf.Remote != "" {
+			remoteAddr = conf.Remote
+		}
+		if conf.BufferSize != 0 {
+			bufferSize = conf.BufferSize
+		}
+	}
+
+	if remoteAddr == "" {
 		fmt.Fprintf(os.Stderr, "Error: serviceString is empty.\n")
 		fmt.Fprintf(os.Stderr, "Try using \"-s <service string>\" flag.\n")
 		os.Exit(1)
 	}
-	fmt.Fprintln(os.Stderr, "Service string:", serviceString)
+	fmt.Fprintln(os.Stderr, "Service string:", remoteAddr)
 	/*go func() {
 		time.Sleep(400 * time.Second)
 		pprof.Lookup("goroutine").WriteTo(os.Stdout, 1)
 		panic("stop")
 	}()*/
 
-	server := proxy.NewProxyServer(password, serviceString, bufferSize)
+	server := proxy.NewProxyServer(password, remoteAddr, bufferSize)
 	server.Run()
 }
